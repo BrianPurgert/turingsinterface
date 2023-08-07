ded6d00d1f7be9dea3aff1d0c9eb0efba37268dd
Brian Purgert <brianpurgert2@gmail.com>
2023-08-06 22:04:17 -0400

    gpt

--- app.vue
+++ app.vue
@@ -1,23 +1,58 @@
-<template>
-	  <UContainer class="mt-4">
-			<UCard>
-				  <UInput v-model="lookupUrl" @keyup.enter="lookup" />
-				  <UButton variant="outline">button</UButton>
-				  <p>This is the body with a <UBadge>Badge</UBadge></p>
-			</UCard>
-	  </UContainer>
-</template>
+<template >
+	  <UContainer class = "mt-4" >
+			<UCard >
+				  <UInput v-model = "lookupUrl" @keyup.enter = "lookup" />
+				  <UButton variant = "outline" >button</UButton >
 
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios' // You'll need to install and import axios to make HTTP requests
 
-const lookupUrl = ref('')
+				  <template >
+						<UForm
+							  ref = "form"
+							  :schema = "schema"
+							  :state = "state"
+							  @submit.prevent = "submit"
+						>
+							  <UFormGroup label = "Email" name = "email" >
+									<UInput v-model = "state.email" />
+							  </UFormGroup >
 
-const lookup = async () => {
-	if (lookupUrl.value) {
-		const response = await axios.get(`http://dev.brianpurgert2.com:3000/?url=${encodeURIComponent(lookupUrl.value)}`)
-		// handle the response here
-	}
+							  <UFormGroup label = "Password" name = "password" >
+									<UInput v-model = "state.password" type = "password" />
+							  </UFormGroup >
+
+							  <UButton type = "submit" >
+									Submit
+							  </UButton >
+						</UForm >
+				  </template >
+
+
+			</UCard >
+	  </UContainer >
+</template >
+
+<script setup lang = "ts" >
+import { object, string, InferType } from 'yup'
+
+const schema = object({
+	email: string().email('Invalid email').required('Required'),
+	password: string()
+		.min(8, 'Must be at least 8 characters')
+		.required('Required')
+})
+
+const state = ref({
+	email: undefined,
+	password: undefined
+})
+
+const form = ref()
+
+async function submit() {
+	await form.value!.validate()
+	// Do something with state.value
+	// 		fetch
 }
-</script>
+
+
+</script >
