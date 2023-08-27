fff26cc1c218e29e638ac500b37059dc0b746591
Brian Purgert <brianpurgert2@gmail.com>
2023-08-27 08:03:23 -0400

    Remove package-lock.json for turingsinterface project
    Avoids merge conflicts in package-lock.json produced by NPM by deleting the file from the repository. The file will be generated locally on npm install and provides noise in the repository's git history. It's also common to encounter merge conflicts in this file.

--- app.vue
+++ app.vue
@@ -1,23 +1,48 @@
-<template>
-	  <UContainer class="mt-4">
-			<UCard>
-				  <UInput v-model="lookupUrl" @keyup.enter="lookup" />
-				  <UButton variant="outline">button</UButton>
-				  <p>This is the body with a <UBadge>Badge</UBadge></p>
-			</UCard>
-	  </UContainer>
-</template>
+<template >
+	  <UContainer class = "mt-4" >
+			<UCard >
+				  <UInput
+						v-model = "lookupUrl"
+						icon = 'i-heroicons-document-arrow-down'
+						:loading = "false"
+						trailing
+						placeholder = "https://website.com/products?keys=values..."
+						@keyup.enter = "lookup"
+				  />
+				  <UButton variant = "outline" >button</UButton >
+				  <p >This is the body with a
+						<UBadge >Badge</UBadge >
+				  </p >
+			</UCard >
+	  </UContainer >
+</template >
 
-<script setup>
-import { ref } from 'vue'
-import axios from 'axios' // You'll need to install and import axios to make HTTP requests
+<script setup >
 
-const lookupUrl = ref('')
-
-const lookup = async () => {
-	if (lookupUrl.value) {
-		const response = await axios.get(`http://dev.brianpurgert2.com:3000/?url=${encodeURIComponent(lookupUrl.value)}`)
-		// handle the response here
-	}
-}
-</script>
+</script >
+<!-- "gsin": {
+        "12000012828225": {
+            "url": "/advantage/ws/catalog/product_detail?gsin=12000012828225",
+            "content": [
+                "5049D5U",
+                "ThinkCentre M81 5049D5U Desktop Computer",
+                "Mfr: Lenovo Group Limited",
+                "from",
+                "$",
+                "1,000.12",
+                "From 14 source",
+                "s",
+                " ",
+                "Includes:",
+                "d",
+                "w",
+                "o",
+                "wo",
+                "ew",
+                "8a",
+                "dv",
+                "h",
+                "Description",
+                "Premium performers. Efficient cost-cutters. Convenient space-savers. Whatever your need, we have the professional-strength desktops to meet it. All designed to simplify management,..."
+            ]
+        },-->
