7e31d6a7557bcbedb5894481a0f5e4aced308642
Brian Purgert <brianpurgert2@gmail.com>
2023-09-04 14:52:31 -0400

    Remove unused files and update dependencies
    Unused files and workflows were deleted to have a cleaner and more manageable project structure. As for the updates, the package.json was updated for its dependencies to have the latest version. Also, removed the unnecessary frozen string literal from the Gemfile and fixed the directory mapping from the .idea/vcs.xml and .idea/deployment.xml. Minor changes were also made to several other files to improve the coding and implementation logic. These changes were made to keep the project up-to-date and ensure that it is using the most recent versions of its dependencies, thus leveraging the latest features and bug fixes. Also, deleting unnecessary files helps to maintain a more uncluttered and manageable codebase.

--- app.vue
+++ app.vue
@@ -1,48 +1,98 @@
 <template >
 	  <UContainer class = "mt-4" >
 			<UCard >
+				  <UAccordion :items = "items" >
+						<template #item = "{ item }" >
+							  <p class = "italic text-gray-900 dark:text-white text-center" >
+									{{ item.description }} </p >
+						</template >
+						<template #getting-started >
+							  <div class = "flex flex-col justify-center items-center gap-1" >
+									<NuxtLink
+										  to = "/getting-started"
+										  class = "flex items-end gap-1.5 font-bold text-xl text-gray-900 dark:text-white"
+									>
+										  <Logo class = "w-8 h-8 text-primary-500 dark:text-primary-400" />
+										  <span class = "hidden sm:block" >NuxtLabs</span ><span
+										  class = "sm:text-primary-500 dark:sm:text-primary-400" >UI</span >
+									</NuxtLink >
+									<p class = "text-sm text-gray-500 dark:text-gray-400" >
+										  Fully styled and customizable components for Nuxt. </p >
+							  </div >
+						</template >
+						<template #installation = "{ description }" >
+							  <div class = "flex flex-col justify-center items-center gap-1 mb-4" >
+									<h3 class = "text-xl font-bold text-gray-900 dark:text-white" >
+										  Installation </h3 >
+									<p class = "text-sm text-gray-500 dark:text-gray-400" >
+										  Install <code >@nuxthq/ui</code > dependency to your project: </p >
+									<p >
+										  {{ description }} </p >
+							  </div >
+							  <div class = "flex flex-col items-center" >
+									<code >$ npm install @nuxtlabs/ui</code >
+									<code >$ nnpm install -D @nuxthq/ui</code >
+									<code >$ pnpm i -D @nuxthq/ui</code >
+							  </div >
+						</template >
+				  </UAccordion >
+
+				  <span >URL:   {{ df_url }} </span >
+				  <pre >{{ datafile }} </pre >
+				  <span >pending: {{ pending }} </span >
+				  <span >error:{{ error }} </span >
+			</UCard >
+			<UCard >
 				  <UInput
-						v-model = "lookupUrl"
-						icon = 'i-heroicons-document-arrow-down'
-						:loading = "false"
-						trailing
-						placeholder = "https://website.com/products?keys=values..."
-						@keyup.enter = "lookup"
-				  />
-				  <UButton variant = "outline" >button</UButton >
-				  <p >This is the body with a
-						<UBadge >Badge</UBadge >
-				  </p >
+						v-model = "df_url"
+						name = "df_url"
+						type = "url"
+						placeholder = "https://..."
+						icon = "i-heroicons-globe-alt"
+						:ui = "{ icon: { trailing: { pointer: '' } } }"
+				  >
+						<template #trailing >
+							  <UButton
+									color = "blue"
+									@click = "execute"
+
+							  >Fetch
+							  </UButton >
+
+							  <UButton
+									v-show = "df_url !== ''"
+									color = "gray"
+									variant = "link"
+									icon = "i-heroicons-x-mark-20-solid"
+									:padded = "false"
+									@click = "df_url = ''"
+							  />
+						</template >
+				  </UInput >
 			</UCard >
 	  </UContainer >
+
 </template >
 
-<script setup >
+<script setup lang = "ts" >
+const items = [{
+		label: 'Getting Started', icon: 'i-heroicons-information-circle', defaultOpen: true, slot: 'getting-started'
+}, {
+		label: 'Installation', icon: 'i-heroicons-arrow-down-tray', defaultOpen: true, slot: 'installation'
+}, {
+		label: 'Theming', icon: 'i-heroicons-eye-dropper', defaultOpen: true, description: ''
+}]
+const df_url = ref('')
+
+
+const {data: datafile, pending, error, execute} = await useFetch('http://dev.brianpurgert2.com/api', {
+		query: {url: df_url}, immediate: false, watch: false
+})
+
+
+const fetchData = () => {
+		execute()
+}
+
 
 </script >
-<!-- "gsin": {
-        "12000012828225": {
-            "url": "/advantage/ws/catalog/product_detail?gsin=12000012828225",
-            "content": [
-                "5049D5U",
-                "ThinkCentre M81 5049D5U Desktop Computer",
-                "Mfr: Lenovo Group Limited",
-                "from",
-                "$",
-                "1,000.12",
-                "From 14 source",
-                "s",
-                " ",
-                "Includes:",
-                "d",
-                "w",
-                "o",
-                "wo",
-                "ew",
-                "8a",
-                "dv",
-                "h",
-                "Description",
-                "Premium performers. Efficient cost-cutters. Convenient space-savers. Whatever your need, we have the professional-strength desktops to meet it. All designed to simplify management,..."
-            ]
-        },-->
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
-		modules: ['@nuxthq/ui', '@nuxt/devtools']
+  modules: ['@nuxthq/ui', '@nuxt/devtools'],
+
+  devtools: {
+	  timeline: {
+		  enabled: true
+		}
+	}
 })
